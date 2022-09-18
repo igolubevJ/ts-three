9e48455502b2b258892356d6de5e96ff9b18d611
igolubevJ <igolubevapp@gmail.com>
2022-09-18 16:42:51 +0300

    add camera folder, and props to camera z position

--- src/client/client.ts
+++ src/client/client.ts
@@ -48,6 +48,10 @@ cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
 cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
 cubeFolder.open();
 
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 20);
+cameraFolder.open();
+
 function animate() {
   requestAnimationFrame(animate);
 
