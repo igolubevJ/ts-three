8721f58c25cedf38db468cf74d9fd324eb890f45
igolubevJ <igolubevapp@gmail.com>
2022-09-18 18:48:16 +0300

    add to gui cube props

--- src/client/client.ts
+++ src/client/client.ts
@@ -23,7 +23,7 @@ document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
 
-const boxGeometry = new THREE.BoxGeometry(1, 2, 3, 4, 5, 6);
+const boxGeometry = new THREE.BoxGeometry();
 
 console.log(boxGeometry);
 
@@ -48,23 +48,56 @@ document.body.appendChild(stats.dom);
 
 const gui = new GUI();
 
-const cubeFolder = gui.addFolder('Cube')
-const cubeRotationFolder = cubeFolder.addFolder('Rotation')
-cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01)
-cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01)
-cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01)
+const cubeFolder = gui.addFolder('Cube');
+const cubeRotationFolder = cubeFolder.addFolder('Rotation');
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2, 0.01);
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2, 0.01);
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2, 0.01);
 
-const cubePositionFolder = cubeFolder.addFolder('Position')
-cubePositionFolder.add(cube.position, 'x', -10, 10)
-cubePositionFolder.add(cube.position, 'y', -10, 10)
-cubePositionFolder.add(cube.position, 'z', -10, 10)
+const cubePositionFolder = cubeFolder.addFolder('Position');
+cubePositionFolder.add(cube.position, 'x', -10, 10);
+cubePositionFolder.add(cube.position, 'y', -10, 10);
+cubePositionFolder.add(cube.position, 'z', -10, 10);
 
-const cubeScaleFolder = cubeFolder.addFolder('Scale')
-cubeScaleFolder.add(cube.scale, 'x', -5, 5, 0.1) //.onFinishChange(() => console.dir(cube.geometry))
-cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1)
-cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1)
-cubeFolder.add(cube, 'visible', true)
-cubeFolder.open()
+const cubeScaleFolder = cubeFolder.addFolder('Scale');
+cubeScaleFolder.add(cube.scale, 'x', -5, 5, 0.1); //.onFinishChange(() => console.dir(cube.geometry))
+cubeScaleFolder.add(cube.scale, 'y', -5, 5, 0.1);
+cubeScaleFolder.add(cube.scale, 'z', -5, 5, 0.1);
+cubeFolder.add(cube, 'visible', true);
+cubeFolder.open();
+
+const cubeData = {
+    width: 1,
+    height: 1,
+    depth: 1,
+    widthSegments: 1,
+    heightSegments: 1,
+    depthSegments: 1,
+};
+
+const cubePropertiesFolder = cubeFolder.addFolder('Properties')
+cubePropertiesFolder
+    .add(cubeData, 'width', 1, 30)
+    .onChange(regenerateBoxGeometry)
+    .onFinishChange(() => console.dir(cube.geometry))
+cubePropertiesFolder.add(cubeData, 'height', 1, 30).onChange(regenerateBoxGeometry)
+cubePropertiesFolder.add(cubeData, 'depth', 1, 30).onChange(regenerateBoxGeometry)
+cubePropertiesFolder.add(cubeData, 'widthSegments', 1, 30).onChange(regenerateBoxGeometry)
+cubePropertiesFolder.add(cubeData, 'heightSegments', 1, 30).onChange(regenerateBoxGeometry)
+cubePropertiesFolder.add(cubeData, 'depthSegments', 1, 30).onChange(regenerateBoxGeometry)
+
+function regenerateBoxGeometry() {
+    const newGeometry = new THREE.BoxGeometry(
+        cubeData.width,
+        cubeData.height,
+        cubeData.depth,
+        cubeData.widthSegments,
+        cubeData.heightSegments,
+        cubeData.depthSegments
+    )
+    cube.geometry.dispose()
+    cube.geometry = newGeometry
+}
 
 const cameraFolder = gui.addFolder("Camera");
 cameraFolder.add(camera.position, "z", 0, 20);
@@ -76,7 +109,7 @@ function animate() {
   requestAnimationFrame(animate);
   render();
 
-  // debug.innerText = 'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n')
+  debug.innerText = 'Matrix\n' + cube.matrix.elements.toString().replace(/,/g, '\n')
     
   stats.update();
 }
