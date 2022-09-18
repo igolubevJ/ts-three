5eebedb07b6fc91feb4d0bb4d3623015a7d7748c
igolubevJ <igolubevapp@gmail.com>
2022-09-18 14:59:57 +0300

    Disable animate function and use render func. OrbitContol does not working

--- src/client/client.ts
+++ src/client/client.ts
@@ -38,14 +38,15 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
 
-  render();
+  // render();
 }
 
 function render() {
   renderer.render(scene, camera);
 }
 
-animate();
+// animate();
+render();
