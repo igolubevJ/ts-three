fa8fb34991dccba8befc071ec652109e1bb50243
igolubevJ <igolubevapp@gmail.com>
2022-09-18 17:25:43 +0300

    clear cube

--- src/client/client.ts
+++ src/client/client.ts
@@ -22,14 +22,6 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 // controls.addEventListener('change', render);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,  
-});
-
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -43,28 +35,6 @@ const stats = Stats();
 document.body.appendChild(stats.dom);
 
 const gui = new GUI();
-const cubeFolder = gui.addFolder("Cube");
-cubeFolder.open();
-
-const cubeRotationFolder = cubeFolder.addFolder("Rotation")
-cubeRotationFolder.add(cube.rotation, "x", 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, "y", 0, Math.PI * 2);
-cubeRotationFolder.add(cube.rotation, "z", 0, Math.PI * 2);
-cubeRotationFolder.open();
-
-const cubePositionFolder = cubeFolder.addFolder("Position");
-cubePositionFolder.add(cube.position, "x", -20, 20, 0.1);
-cubePositionFolder.add(cube.position, "y", -20, 20, 0.1);
-cubePositionFolder.add(cube.position, "z", -20, 20, 0.1);
-cubePositionFolder.open();
-
-const cubeScaleFolder = cubeFolder.addFolder("Scale");
-cubeScaleFolder.add(cube.scale, "x", -10, 10);
-cubeScaleFolder.add(cube.scale, "y", -10, 10);
-cubeScaleFolder.add(cube.scale, "z", -10, 10);
-cubeScaleFolder.open();
-
-cubeFolder.add(cube, "visible")
 
 const cameraFolder = gui.addFolder("Camera");
 cameraFolder.add(camera.position, "z", 0, 20);
