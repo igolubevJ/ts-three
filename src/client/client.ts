0cf4139778fa182b46b1f3582b912c7482404e5c
igolubev <igolubevapp@gmail.com>
2022-09-20 14:13:16 +0300

    add light to scene

--- src/client/client.ts
+++ src/client/client.ts
@@ -5,6 +5,10 @@ import Stats from 'three/examples/jsm/libs/stats.module';
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
 
+const light = new THREE.PointLight();
+light.position.set(2.5, 7.5, 15);
+scene.add(light);
+
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.z = 3;
 
