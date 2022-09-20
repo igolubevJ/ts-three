e52a7238e09d20d1a90b2d33ac3aad6862e896e0
igolubevJ <igolubevapp@gmail.com>
2022-09-20 20:33:29 +0300

    setup controls and add light

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,9 +1,17 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
+
+const light = new THREE.PointLight();
+light.position.set(0.8, 1.4, 1.0);
+scene.add(light);
+
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.z = 3;
@@ -15,7 +23,9 @@ renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-new OrbitControls(camera, renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.target.set(0, 1, 0);
 
 const backgroundTexture = new THREE.CubeTextureLoader().load([
   'img/px_eso0932a.jpg',
@@ -40,6 +50,8 @@ document.body.appendChild(stats.dom);
 
 function animate() {
   requestAnimationFrame(animate);
+
+  controls.update();
 
   render();
 
