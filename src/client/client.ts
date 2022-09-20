feff5d671fde42c7569fae13341146f62345ecc3
igolubevJ <igolubevapp@gmail.com>
2022-09-20 20:23:02 +0300

    create example

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,9 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
-
-// const light = new THREE.PointLight();
-// light.position.set(0, 7.5, 15);
-// scene.add(light);
-
-// const light = new THREE.SpotLight();
-// light.position.set(5, 5, 5);
-// scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.z = 3;
@@ -26,35 +16,6 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
-
-const dracoLoader = new DRACOLoader();
-dracoLoader.setDecoderPath('js/libs/draco/');
-
-const loader = new GLTFLoader();
-loader.setDRACOLoader(dracoLoader);
-loader.load(
-  'models/monkey_compressed.glb',
-  function (gltf) {
-    gltf.scene.traverse(function (child) {
-      if ((child as THREE.Mesh).isMesh) {
-        const m = <THREE.Mesh>child;
-        m.receiveShadow = true;
-        m.castShadow = true;
-      }
-
-      if ((child as THREE.Light).isLight) {
-        const l = <THREE.Light>child;
-        l.castShadow = true;
-        l.shadow.bias = -0.003;
-        l.shadow.mapSize.width = 2048;
-        l.shadow.mapSize.height = 2048;
-      }
-    });
-    scene.add(gltf.scene);
-  },
-  (xhr) => console.log(((xhr.loaded / xhr.total) * 100) + '% loaded'),
-  (err) => console.log(err)
-);
 
 const backgroundTexture = new THREE.CubeTextureLoader().load([
   'img/px_eso0932a.jpg',
