4cfd85cb0e2083178bedcf854955e6c386ae8356
igolubev <igolubevapp@gmail.com>
2022-09-20 16:26:17 +0300

    Create example

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
@@ -18,38 +18,10 @@ const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 10
 camera.position.z = 3;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.physicallyCorrectLights = true;
-renderer.shadowMap.enabled = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 new OrbitControls(camera, renderer.domElement);
-
-const loader = new GLTFLoader();
-loader.load(
-  'models/monkey.glb',
-  function (gltf) {
-    gltf.scene.traverse(function (child) {
-      if ((child as THREE.Mesh).isMesh) {
-        const m = child as THREE.Mesh;
-        m.receiveShadow = true;
-        m.castShadow = true;
-      }
-
-      if((child as THREE.Light).isLight) {
-        const l = child as THREE.Light;
-        l.castShadow = true;
-        l.shadow.bias = -0.003;
-        l.shadow.mapSize.width = 2048;
-        l.shadow.mapSize.height = 2048;
-      }
-    });
-
-    scene.add(gltf.scene);
-  },
-  (xhr) => console.log(((xhr.loaded / xhr.total) * 100) + '% loaded'),
-  (err) => console.log(err)
-);
 
 
 const backgroundTexture = new THREE.CubeTextureLoader().load([
