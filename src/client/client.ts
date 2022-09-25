7f26eb6f402297c3050b6074fc5453d948cfd023
igolubevJ <igolubevapp@gmail.com>
2022-09-25 21:21:51 +0300

    create example

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,14 +7,6 @@ import { GUI } from 'dat.gui';
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
 
-const light1 = new THREE.PointLight();
-light1.position.set(2.5, 2.5, 2.5);
-scene.add(light1);
-
-const light2 = new THREE.PointLight();
-light2.position.set(-2.5, 2.5, 2.5);
-scene.add(light2);
-
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(0.8, 1.4, 1.0);
 
@@ -26,69 +18,6 @@ const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.target.set(0, 1, 0);
 
-let mixer: THREE.AnimationMixer;
-let modelReady = false;
-const animationActions: THREE.AnimationAction[] = [];
-let activeAction: THREE.AnimationAction;
-let lastAction: THREE.AnimationAction
-const gltfLoader = new GLTFLoader();
-
-gltfLoader.load(
-  'models/eve.glb',
-  (gltf) => {
-    mixer = new THREE.AnimationMixer(gltf.scene);
-
-    const animationAction = mixer.clipAction((<any>gltf).animations[0]);
-    animationActions.push(animationAction);
-    animationsFolder.add(animations, 'default');
-    activeAction = animationActions[0];
-
-    scene.add(gltf.scene);
-
-    // add animations another file
-    gltfLoader.load(
-      'models/eve-angry.glb',
-      (gltf) => {
-        const animationAction = mixer.clipAction(
-          (<any>gltf).animations[0]
-        );
-        animationActions.push(animationAction);
-        animationsFolder.add(animations, "angry");
-
-        gltfLoader.load(
-          'models/eve-clapping.glb',
-          (gltf) => {
-            const animationAction = mixer.clipAction(
-              (<any>gltf).animations[0]
-            );
-            animationActions.push(animationAction);
-            animationsFolder.add(animations, "clapping");
-
-            modelReady = true;
-          },
-          (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + '% loaded clapping');
-          },
-          (err) => {
-            console.log(err);
-          }
-        );
-      },
-      (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded angry');
-      },
-      (err) => {
-        console.log(err);
-      }
-    )
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-  },
-  (err) => {
-    console.log(err);
-  }
-)
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -101,33 +30,6 @@ function onWindowResize() {
 const stats = Stats();
 document.body.appendChild(stats.dom);
 
-const animations = {
-  default: function () {
-    setAction(animationActions[0]);
-  },
-  angry: function () {
-    setAction(animationActions[1]);
-  },
-  clapping: function () {
-    setAction(animationActions[2]);
-  }, 
-  dancing: function () {
-    setAction(animationActions[3]);
-  }
-}
-
-function setAction(toAction: THREE.AnimationAction) {
-  if (toAction !== activeAction) {
-    lastAction = activeAction;
-    activeAction = toAction;
-    //lastAction.stop();
-    lastAction.fadeOut(1);
-    activeAction.reset();
-    activeAction.fadeIn(1);
-    activeAction.play();
-  }
-}
-
 const gui = new GUI();
 const animationsFolder = gui.addFolder("Animation");
 animationsFolder.open();
@@ -138,8 +40,6 @@ function animate() {
   requestAnimationFrame(animate);
 
   controls.update();
-
-  if (modelReady) mixer.update(clock.getDelta());
 
   render();
 
