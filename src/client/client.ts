fbda9d19022863c44e29f737ab4696d4782080bb
igolubevJ <igolubevapp@gmail.com>
2022-09-25 20:22:56 +0300

    create light

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { GUI } from 'dat.gui';
 
@@ -7,10 +8,13 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87b8ce);
 scene.add(new THREE.AxesHelper(5));
 
-const light = new THREE.PointLight();
-light.position.set(2.5, 7.5, 15);
-light.intensity = 2.5;
-scene.add(light);
+const light1 = new THREE.PointLight();
+light1.position.set(2.5, 2.5, 2.5);
+scene.add(light1);
+
+const light2 = new THREE.PointLight();
+light2.position.set(-2.5, 2.5, 2.5);
+scene.add(light2);
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(0.8, 1.4, 1.0);
