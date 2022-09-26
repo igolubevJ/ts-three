b8032f30594b56a72459a8fbcb70078e0edf19eb
igolubev <igolubevapp@gmail.com>
2022-09-26 10:02:55 +0300

    add drag controls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { DragControls } from 'three/examples/jsm/controls/DragControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
@@ -27,6 +28,34 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.target.set(0, 1, 0);
+
+const sceneMeshes: THREE.Mesh[] = [];
+let boxHelper: THREE.BoxHelper;
+
+const dragControls = new DragControls(sceneMeshes, camera, renderer.domElement);
+dragControls.addEventListener('hoveron', function() {
+  boxHelper.visible = true;
+  controls.enabled = false;
+});
+
+dragControls.addEventListener('hoveroff', function() {
+  boxHelper.visible = false;
+  controls.enabled = true;
+});
+
+dragControls.addEventListener('drag', function(event) {
+  event.object.position.y = 0;
+});
+
+dragControls.addEventListener('dragstart', function() {
+  boxHelper.visible = true;
+  controls.enabled = false;
+});
+
+dragControls.addEventListener('dragend', function() {
+  boxHelper.visible = false;
+  controls.enabled = true;
+});
 
 const planeGeometry = new THREE.PlaneGeometry(25, 25);
 const texture = new THREE.TextureLoader().load('img/grid.png');
