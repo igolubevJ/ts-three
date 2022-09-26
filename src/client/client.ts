8ecfbea493230d0b211813029f1ce65e47508d7e
igolubev <igolubevapp@gmail.com>
2022-09-26 09:36:01 +0300

    add axes helper

--- src/client/client.ts
+++ src/client/client.ts
@@ -4,6 +4,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(4.0, 4.0, 4.0);
