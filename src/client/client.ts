214e4899becadb881a21ff4f8ea19544156c5206
igolubev <igolubevapp@gmail.com>
2022-09-26 09:39:28 +0300

    add lights

--- src/client/client.ts
+++ src/client/client.ts
@@ -6,6 +6,16 @@ import Stats from 'three/examples/jsm/libs/stats.module';
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
 
+const light1 = new THREE.PointLight();
+light1.position.set(2.5, 2.5, 2.5);
+light1.castShadow = true;
+scene.add(light1);
+
+const light2 = new THREE.PointLight();
+light2.position.set(-2.5, 2.5, 2.5);
+light2.castShadow = true;
+scene.add(light2);
+
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 camera.position.set(4.0, 4.0, 4.0);
 
