3e692f18b6b7dd6af71086fe4e04fe4f80be8f6a
igolubev <igolubevapp@gmail.com>
2022-09-26 11:05:12 +0300

    create example

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DragControls } from 'three/examples/jsm/controls/DragControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
@@ -32,31 +31,6 @@ controls.target.set(0, 1, 0);
 const sceneMeshes: THREE.Mesh[] = [];
 let boxHelper: THREE.BoxHelper;
 
-const dragControls = new DragControls(sceneMeshes, camera, renderer.domElement);
-dragControls.addEventListener('hoveron', function() {
-  boxHelper.visible = true;
-  controls.enabled = false;
-});
-
-dragControls.addEventListener('hoveroff', function() {
-  boxHelper.visible = false;
-  controls.enabled = true;
-});
-
-dragControls.addEventListener('drag', function(event) {
-  event.object.position.y = 0;
-});
-
-dragControls.addEventListener('dragstart', function() {
-  boxHelper.visible = true;
-  controls.enabled = false;
-});
-
-dragControls.addEventListener('dragend', function() {
-  boxHelper.visible = false;
-  controls.enabled = true;
-});
-
 const planeGeometry = new THREE.PlaneGeometry(25, 25);
 const texture = new THREE.TextureLoader().load('img/grid.png');
 const plane: THREE.Mesh = new THREE.Mesh(
@@ -76,32 +50,11 @@ let modelDragBox: THREE.Mesh;
 gltfLoader.load('models/eve.@pounchglb.glb', 
   (gltf) => {
     gltf.scene.traverse(function(child) {
-      if (child instanceof THREE.Group) {
-        modelGroup = child;
-      }
-
-      if ((child as THREE.Mesh).isMesh) {
-        child.castShadow = true;
-        child.frustumCulled = false;
-        (child as THREE.Mesh).geometry.computeVertexNormals();
-      }
+      
     });
 
     mixer = new THREE.AnimationMixer(gltf.scene);
     mixer.clipAction((gltf as any).animations[0]).play();
-
-    modelDragBox = new THREE.Mesh(
-      new THREE.BoxGeometry(0.5, 1.3, 0.5),
-      new THREE.MeshBasicMaterial({ transparent: true, opacity: 0 })
-    );
-
-    modelDragBox.geometry.translate(0, 0.65, 0);
-    scene.add(modelDragBox);
-    sceneMeshes.push(modelDragBox);
-
-    boxHelper = new THREE.BoxHelper(modelDragBox, 0xffff00);
-    boxHelper.visible = false;
-    scene.add(boxHelper);
 
     scene.add(gltf.scene);
     modelReady = true;
@@ -130,8 +83,6 @@ function animate() {
 
   if (modelReady) {
     mixer.update(clock.getDelta());
-    modelGroup.position.copy(modelDragBox.position);
-    boxHelper.update();
   }
 
   render();
