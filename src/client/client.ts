f331b5e125811f83f66cbe661fdce9c40dd19b77
igolubevJ <igolubevapp@gmail.com>
2022-09-27 06:18:38 +0300

    add light

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,20 @@
 import * as THREE from 'three';
+import { AxesHelper } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
+scene.add(new AxesHelper(5));
+
+const light = new THREE.SpotLight();
+light.position.set(12.5, 12.5, 12.5);
+light.castShadow = true;
+light.shadow.mapSize.width = 1024;
+light.shadow.mapSize.height = 1024;
+scene.add(light);
 
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-camera.position.z = 2;
+camera.position.set(15, 15, 15);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
