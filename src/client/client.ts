d9b49d52a72d8d93a6de36b042038f7cecba31d1
igolubevJ <igolubevapp@gmail.com>
2022-09-27 03:33:37 +0300

    create example

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,6 +1,5 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 
 const scene = new THREE.Scene();
@@ -10,61 +9,13 @@ const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 10
 camera.position.z = 2;
 
 const renderer = new THREE.WebGLRenderer();
-renderer.physicallyCorrectLights = true;
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
 document.body.appendChild(renderer.domElement);
-
-let arrowHelper = new THREE.ArrowHelper(
-  new THREE.Vector3(),
-  new THREE.Vector3(),
-  0.25,
-  0xffff00
-);
-scene.add(arrowHelper);
-
-const material = new THREE.MeshNormalMaterial();
-
-// const boxGeometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-const coneGeometry = new THREE.ConeGeometry(0.05, 0.2, 8);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
 const raycaster = new THREE.Raycaster();
-const sceneMeshes: THREE.Object3D[] = [];
-
-const gltfLoader = new GLTFLoader();
-
-gltfLoader.load('models/monkey_textured.glb', 
-  (gltf) => {
-    gltf.scene.traverse(function (child) {
-      if ((child as THREE.Mesh).isMesh) {
-        let m = child as THREE.Mesh;
-        m.receiveShadow = true;
-        m.castShadow = true;
-        (m.material as THREE.MeshStandardMaterial).flatShading = true;
-        // if (m.userData.name !== 'Plane') {
-        //   sceneMeshes.push(m);
-        // }
-      }
-
-      if ((child as THREE.Light).isLight) {
-        let l = child as THREE.Light;
-        l.castShadow = true;
-        l.shadow.bias = -0.003;
-        l.shadow.mapSize.width = 2048;
-        l.shadow.mapSize.height = 2048;
-      }
-    });
-
-    scene.add(gltf.scene);
-    sceneMeshes.push(gltf.scene);
-  },
-  (xhr) => console.log(((xhr.loaded / xhr.total) * 100) + '% loaded'), 
-  (error) => console.log(error)
-);
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -72,54 +23,6 @@ function onWindowResize() {
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
   render();
-}
-
-renderer.domElement.addEventListener('dblclick', onDoubleClick, false);
-renderer.domElement.addEventListener('mousemove', onMouseMove, false);
-
-function onMouseMove(event: MouseEvent) {
-  const mouse = new THREE.Vector2(); 
-  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects(sceneMeshes, true);
-
-  if (intersects.length > 0) {
-    const n = new THREE.Vector3();
-    n.copy((intersects[0].face as THREE.Face).normal);
-    n.transformDirection(intersects[0].object.matrixWorld);
-
-    arrowHelper.setDirection(n);
-    arrowHelper.position.copy(intersects[0].point);
-  }
-}
-
-function onDoubleClick(event: MouseEvent) {
-  const mouse = new THREE.Vector2();
-  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects(sceneMeshes, true);
-
-  if (intersects.length > 0) {
-    const n = new THREE.Vector3();
-    n.copy((intersects[0].face as THREE.Face).normal);
-    n.transformDirection(intersects[0].object.matrixWorld);
-
-    const cube = new THREE.Mesh(coneGeometry, material);
-
-    cube.lookAt(n);
-    cube.rotateX(Math.PI / 2);
-    cube.position.copy(intersects[0].point);
-    cube.position.addScaledVector(n, 0.1);
-
-    scene.add(cube);
-    sceneMeshes.push(cube);
-  }
 }
 
 const stats = Stats();
