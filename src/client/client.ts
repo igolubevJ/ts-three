76bf6ff3a8059861681395826c2c8d3557695b47
igolubev <igolubevapp@gmail.com>
2022-11-07 14:04:36 +0300

    Create empty scene

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,51 +2,24 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'dat.gui';
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min';
 
 const scene = new THREE.Scene();
+scene.add(new THREE.AxesHelper(5));
 
 const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
+  75, window.innerWidth / window.innerHeight, 0.1, 1000
 );
-camera.position.set(15, 15, 15);
+camera.position.set(0.8, 1.5, 1.0);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.physicallyCorrectLights = true;
-renderer.shadowMap.enabled = true;
-renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-
-const sceneMeshes: THREE.Mesh[] = [];
-
-const loader = new GLTFLoader();
-loader.load('models/monkey_textured.glb', (gltf) => {
-  gltf.scene.traverse(function(child) {
-    if ((child as THREE.Mesh).isMesh) {
-      const m = child as THREE.Mesh;
-      m.receiveShadow = true;
-      m.castShadow = true;
-      sceneMeshes.push(m);
-    }
-
-    if ((child as THREE.Light).isLight) {
-      const l = child as THREE.Light;
-      l.castShadow = true;
-      l.shadow.bias = -0.003;
-      l.shadow.mapSize.width = 2048;
-      l.shadow.mapSize.height = 2048;
-    }
-
-  });
-  scene.add(gltf.scene);
-});
+controls.target.set(0, 1, 0);
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -56,76 +29,15 @@ function onWindowResize() {
     render();
 }
 
-const raycaster = new THREE.Raycaster();
-
-renderer.domElement.addEventListener('dblclick', onDoubleClick, false);
-function onDoubleClick(event: MouseEvent) {
-  const mouse = new THREE.Vector2();
-  mouse.x = (event.clientX / renderer.domElement.clientWidth) * 2 - 1;
-  mouse.y = -(event.clientY / renderer.domElement.clientHeight) * 2 + 1;
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersects = raycaster.intersectObjects(sceneMeshes, false);
-  if (intersects.length > 0) {
-    const p = intersects[0].point;
-    // controls.target.set(p.x, p.y, p.z);
-    // new TWEEN.Tween(controls.target)
-    //   .to({ x: p.x, y: p.y, z: p.z }, 500)
-    //   // .delay(1000)
-    //   .easing(TWEEN.Easing.Cubic.Out)
-    //   .start();
-
-    new TWEEN.Tween(sceneMeshes[1].position)
-      .to({
-        x: p.x,
-        // y: p.y + 1,
-        z: p.z
-      },
-      500
-    ).start();
-
-    new TWEEN.Tween(sceneMeshes[1].position)
-      .to({
-        // x: p.x,
-        y: p.y + 3,
-        // z: p.z
-      }, 250)
-      .delay(1000)
-      .easing(TWEEN.Easing.Cubic.Out)
-      // .onUpdate(() => render())
-      .start()
-      .onComplete(() => {
-        new TWEEN.Tween(sceneMeshes[1].position)
-          .to(
-            {
-              // x: p.x,
-              y: p.y + 1,
-              // z: p.z
-            },
-            250
-          ).easing(TWEEN.Easing.Bounce.Out)
-          .start();
-      });
-  }
-}
-
-const stats = Stats();
-document.body.appendChild(stats.dom);
-
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate)
 
-    controls.update();
+  controls.update();
 
-    TWEEN.update();
-
-    render();
-
-    stats.update();
+  render();
 }
 
 function render() {
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
-
-animate()
+animate();
